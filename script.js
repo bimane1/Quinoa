f3868cf8a80b6d6c0f6f9ca51fd52bb629e83284
bimane1 <barkat7imane7@gmail.com>
2025-07-06 22:27:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// زر إرسال الاتصال
-const contactForm = document.getElementById("contactForm");
-if (contactForm) {
-  contactForm.addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Thank you! We will contact you soon.");
-    this.reset();
-  });
-}
+
 
 // زر إضافة إلى السلة
 document.querySelectorAll(".btn-add-to-cart").forEach(button => {
