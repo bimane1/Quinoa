919bf34557adb5941b7319efe7972c32382714fc
bimane1 <barkat7imane7@gmail.com>
2025-07-06 22:27:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 
-
-// زر إضافة إلى السلة
-document.querySelectorAll(".btn-add-to-cart").forEach(button => {
-  button.addEventListener("click", () => {
-    button.textContent = "Added!";
-    button.style.backgroundColor = "green";
-    setTimeout(() => {
-      button.textContent = "Add to Cart";
-      button.style.backgroundColor = "#4a8c3e";
-    }, 1500);
-  });
-});
