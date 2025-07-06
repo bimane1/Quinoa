33c61cbd99dcd47fb6a23054ae1542da5b4af0db
bimane1 <barkat7imane7@gmail.com>
2025-07-06 22:26:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// معاينة صورة مرفوعة
-const imageInput = document.getElementById("imageInput");
-const previewImage = document.getElementById("previewImage");
-
-imageInput.addEventListener("change", function () {
-  const file = this.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      previewImage.src = e.target.result;
-      previewImage.style.display = "block";
-    };
-    reader.readAsDataURL(file);
-  }
-});
-
 // زر إرسال الاتصال
 const contactForm = document.getElementById("contactForm");
 if (contactForm) {
